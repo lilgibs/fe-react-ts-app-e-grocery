b5a422e9b7d2f050aadce297204269152870ebbb
Ra <88369275+callraV@users.noreply.github.com>
2023-06-04 23:11:47 +0800

    Update AdminLogin.jsx

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -11,10 +11,6 @@ const AdminLogin = () => {
     password: Yup.string().min(3, "Password haris lebih dari 3 karakter").required("Password tidak boleh kosong"),
   });
 
-  const loginAdmin = (data) => {
-    console.log(data);
-  };
-
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
