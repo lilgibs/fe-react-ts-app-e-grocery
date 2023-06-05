badc34f98ebbeca2ce5c7deb39a168f6d99b9191
Ra <88369275+callraV@users.noreply.github.com>
2023-06-05 21:31:13 +0800

    throw error, server PORT 800
    -menambah throw error
    -port server diubah jadi 8000 krn clash dengan client

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -47,7 +47,7 @@ const AdminLogin = () => {
                   <Field
                     type="text"
                     name="email"
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                   <ErrorMessage component="div" name="email" style={{ color: "red" }} />
                   <div className="mt-2"></div>
@@ -68,7 +68,7 @@ const AdminLogin = () => {
                     <Field
                       type="password"
                       name="password"
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                     <ErrorMessage component="div" name="password" style={{ color: "red" }} />
                   </div>
