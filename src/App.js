fdfaec168d3a2746d0887e7a2f03262c98f6bfe8
Ichsan <ichsannuriman437@gmail.com>
2023-06-06 14:14:11 +0700

    be dan fe fitur register user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import axios from "axios";
-import logo from "./logo.svg";
 import "./styles/App.css";
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
+import Register from "./pages/Register";
 
 function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
     (async () => {
-      const { data } = await axios.get(`${process.env.REACT_APP_API_BASE_URL}/greetings`);
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_BASE_URL}/greetings`
+      );
       setMessage(data?.message || "");
     })();
   }, []);
@@ -21,6 +23,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<AdminLogin />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/adminlogin" element={<AdminLogin />} />
         <Route path="/admindashboard" element={<AdminDashboard />} />
       </Routes>
