b6f8afd6ecd1741e1b87d5252291a82285dd48b7
Ra <88369275+callraV@users.noreply.github.com>
2023-06-25 22:53:23 +0800

    cart slice

---
+++ src/features/cartSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import Axios from "axios";
+import moment from "moment";
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cart: {
+      cart_id: 0,
+      cart_count: 0,
+      cart_item: [],
+    },
+    isLoaded: false,
+  },
+  reducers: {
+    setCart: (state, action) => {
+      state.cart = action.payload;
+    },
+    resetCart: (state) => {
+      state.cart = {
+        cart_id: 0,
+        cart_count: 0,
+        cart_item: [],
+      };
+    },
+    setLoaded: (state, action) => {
+      state.isLoaded = action.payload;
+    },
+  },
+});
+
+export const { setCart, resetCart, setLoaded } = cartSlice.actions;
+export default cartSlice.reducer;
