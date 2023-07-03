b65fe9d84fdcfc167221bbdfb2ae960d2dae3ac1
Ichsan <ichsannuriman437@gmail.com>
2023-07-03 18:24:30 +0700

    fitur not found

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import AdminNavbar from "./components/AdminNavbar";
 import LandingPage from "./pages/LandingPage";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
-import { Route, Routes } from "react-router-dom";
 import UserManagementSettings from "./pages/UserManagementSettings";
 import Register from "./pages/Register";
 import Verification from "./pages/Verification";
 import Login from "./pages/Login";
-import { checkLogin } from "./features/userSlice";
-import { checkLoginAdmin } from "./features/adminSlice";
 import AdminCategories from "./pages/AdminCategories";
 import UserProfile from "./pages/UserProfile";
-import { getCityStore } from "./features/locationSlice";
 import AdminAddProduct from "./pages/AdminAddProduct";
 import AdminProducts from "./pages/AdminProducts";
 import AdminEditProduct from "./pages/AdminEditProduct";
@@ -24,9 +21,13 @@ import Cart from "./pages/Cart";
 import Orders from "./pages/Orders";
 import Products from "./pages/Products";
 import Product from "./pages/Product";
+import ChangePassword from "./pages/ChangePassword";
+import NotFound from "./pages/NotFound";
+import { checkLogin } from "./features/userSlice";
+import { checkLoginAdmin } from "./features/adminSlice";
 import { fetchCart } from "./features/cartSlice";
 import { fetchOrder } from "./features/orderSlice";
-import ChangePassword from "./pages/ChangePassword";
+import { getCityStore } from "./features/locationSlice";
 
 function App() {
   const dispatch = useDispatch();
@@ -57,7 +58,7 @@ function App() {
     if (userToken) {
       dispatch(fetchCart(userGlobal.user_id));
     }
-  }); // get orders if user is logged in
+  }, []);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -91,6 +92,14 @@ function App() {
         </>
       )}
       <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/adminlogin" element={<AdminLogin />} />
+        <Route path="/verification/:token" element={<Verification />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:productName" element={<Product />} />
+
         {adminGlobal.id != null ? (
           //when admin is logged in
           <>
@@ -117,45 +126,19 @@ function App() {
           //when admin is logged out
           <></>
         )}
-
         {userGlobal.user_id != null ? (
           //when user is logged in
           <>
             <Route path="/profile" element={<UserProfile />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/orders" element={<Orders />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/:productName" element={<Product />} />
             <Route path="/change-password" element={<ChangePassword />} />
           </>
         ) : (
           //when user is logged out
-          <>
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/orders" element={<Orders />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/:productName" element={<Product />} />
-          </>
+          <></>
         )}
-
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/adminlogin" element={<AdminLogin />} />
-        <Route path="/verification/:token" element={<Verification />} />
-
-        {/* <Route path="/profile" element={<UserProfile />} /> */
-        /* <Route path="/cart" element={<Cart />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:productName" element={<Product />} /> */
-        /* <Route path="/adminlogin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/admin/settings/users" element={<UserManagementSettings />} />
-        <Route path="/admin/products/categories" element={<AdminCategories />} />
-        <Route path="/admin/products/" element={<AdminProducts />} />
-        <Route path="/admin/products/add-product" element={<AdminAddProduct />} />
-        <Route path="/admin/products/:productId" element={<AdminEditProduct />} /> */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
