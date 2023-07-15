ea15750005911786104dd4e91ec452b79336a2b5
Ra <88369275+callraV@users.noreply.github.com>
2023-07-16 03:17:25 +0800

    admin order filter

--- src/pages/AdminOrders.jsx
+++ src/pages/AdminOrders.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Card, CardHeader, CardBody, CardFooter, Text, Heading, Box, StackDivider } from "@chakra-ui/react";
-import { Button, Stack } from "@chakra-ui/react";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { Button, Text, Divider, Select as ChakraSelect, Input } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 import OrderItem from "../components/OrderItem";
 import { setOrderItems } from "../features/orderSlice";
@@ -59,7 +57,40 @@ const AdminOrders = () => {
 
   // sort by
   const [sortBy, setSortBy] = useState("desc");
-  //......
+
+  const handleSortChange = (event) => {
+    const selectedValue = event.target.value;
+    const orderItems = [...orderGlobal.order_items];
+
+    setSortBy(selectedValue);
+    if (selectedValue === "desc") {
+      const sortDesc = orderItems.sort((a, b) => b.order_id - a.order_id);
+      dispatch(setOrderItems(sortDesc));
+    } else if (selectedValue === "asc") {
+      const sortAsc = orderItems.sort((a, b) => a.order_id - b.order_id);
+      dispatch(setOrderItems(sortAsc));
+    }
+  };
+
+  // id filter
+  const [selectedId, setSelectedId] = React.useState("");
+  const handleIdChange = (event) => setSelectedId(event.target.value);
+
+  const handleId = async () => {
+    try {
+      console.log(selectedId);
+      let response = await axios.get(`http://localhost:8000/api/admin/order/by-invoice/?storeId=${storeId}&orderId=${selectedId}`);
+      if (response.data.data.length === 0) {
+        alert("Order not found");
+      } else {
+        dispatch(setOrderItems(response.data.data));
+        console.log(response.data.data[0].order_status);
+        setSelectedStatus(response.data.data[0].order_status);
+      }
+    } catch (error) {
+      alert(error.response.data);
+    }
+  };
 
   // status filter
   const [selectedStatus, setSelectedStatus] = useState("All");
@@ -88,6 +119,54 @@ const AdminOrders = () => {
       }
     } catch (error) {
       alert(error.response.data);
+    }
+  };
+
+  // date filter
+  const [startValue, setStartValue] = useState("");
+  const [endValue, setEndValue] = useState("");
+  const [minEndDate, setMinEndDate] = useState("");
+
+  const handleStartChange = (e) => {
+    const selectedStartDate = e.target.value;
+    setStartValue(selectedStartDate);
+    setMinEndDate(selectedStartDate); // Set the minimum end date as the selected start date
+    if (endValue < selectedStartDate) {
+      setEndValue(selectedStartDate); // Reset the end date if it's earlier than the new start date
+    }
+  };
+
+  const handleEndChange = (e) => {
+    const selectedEndDate = e.target.value;
+    if (selectedEndDate >= startValue) {
+      setEndValue(selectedEndDate);
+    }
+  };
+
+  const formatDate = (dateString) => {
+    const dateObj = new Date(dateString);
+    const year = dateObj.getFullYear();
+    const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+    const day = String(dateObj.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }; //format date for sql
+
+  const handleDate = async () => {
+    try {
+      const startDate = formatDate(startValue);
+      const endDate = formatDate(endValue);
+
+      const response = await axios.get(`http://localhost:8000/api/admin/order/by-date/?storeId=${storeId}&page=${currentPage}&startDate="${startDate}"&endDate="${endDate}"`);
+
+      console.log(response.data);
+      if (response.data.orders.length == 0) {
+        alert("Orders not found");
+      }
+      dispatch(setOrderItems(response.data.orders));
+      setTotalPages(response.data.maxPages);
+      setSelectedStatus("All");
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -187,6 +266,47 @@ const AdminOrders = () => {
               </div>
             ))}
             {/* status ends */}
+            {/* Filter starts */}
+            <Divider />
+            <Text className="font-semibold text-pink-600">Sort and Filter</Text>
+            <div className="flex flex-col gap-3">
+              <div>
+                <ChakraSelect placeholder="Sort by" size="sm" value={sortBy} onChange={handleSortChange}>
+                  <option value="desc">Latest to oldest</option>
+                  <option value="asc">Oldest to latest</option>
+                </ChakraSelect>
+              </div>
+
+              <div className="grid grid-cols-3 gap-1">
+                <Input className="col-span-2" type="number" value={selectedId} onChange={handleIdChange} placeholder="Find order by id" size="sm" />
+                <Button className="col-span-1" size="sm" onClick={handleId}>
+                  Find
+                </Button>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <div className="grid grid-cols-4">
+                  <Text>Start</Text>
+                  <Input className="col-span-3" placeholder="Select Date and Time" size="sm" type="datetime-local" value={startValue} onChange={handleStartChange} />
+                </div>
+                <div className="grid grid-cols-4">
+                  <Text>End</Text>
+                  <Input
+                    className="col-span-3"
+                    placeholder="Select Date and Time"
+                    size="sm"
+                    type="datetime-local"
+                    value={endValue}
+                    onChange={handleEndChange}
+                    min={minEndDate} // Set the minimum allowed value for the end date
+                  />
+                </div>
+                <Button size="sm" onClick={handleDate}>
+                  Find order by date
+                </Button>
+              </div>
+            </div>
+            {/* Filter ends */}
           </div>
         </div>
 
